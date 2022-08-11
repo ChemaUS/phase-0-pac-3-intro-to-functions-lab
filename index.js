87ab339178e4ef7a8abc94adfa246d81ed0a2631
Cesar Hernandez <chema27281@gmail.com>
2022-08-11 00:17:09 -0400

    commit tree 77205a5e6ec5719af8c1c6a47be16b1758453000

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+function whisper(string) {
+  return string.toLowerCase();
+}
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+  if (string.toLowerCase(string) === string) 
+ return string = "I can't hear you!"
+ else if (string.toUpperCase(string) === string)
+ return string = "YES INDEED!"
+ else if ("Lets have dinner together!" === string);
+ return "I would love to!"
+}
